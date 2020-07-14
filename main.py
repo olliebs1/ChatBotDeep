a452b68701805eb71aad6a64b92d13cb73f23490
Olivier Benson-Smith <olivierbenson-smith@Oliviers-MBP.broadband>
2020-07-14 15:36:52 +0100

    looping through our JSON data and extracting the data we want, saving in correct lists

--- main.py
+++ main.py
@@ -16,3 +16,13 @@ words = []
 labels = []
 docs_x = []
 docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+        
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
