e306a1258136ac89cf093a617cbfb99df171bd25
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-07-15 15:28:37 +0100

    bag of words created - looping through words in new stemmed word list and checking if its there, assinging it a value if it is or isnt

--- main.py
+++ main.py
@@ -37,3 +37,23 @@ training = []
 output= []
 
 out_empty = [0 for _ in range(len(classes))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words: 
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpu.array(training)
+output = np.array(output)
