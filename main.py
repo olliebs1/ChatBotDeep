125976bf9b1165888c0896218561897ff7644748
Olivier Benson-Smith <olliebenson@hotmail.co.uk>
2020-07-15 15:51:25 +0100

    resetting tensorflow graph and then added code to define the architecture of the model

--- main.py
+++ main.py
@@ -57,3 +57,13 @@ for x, doc in enumerate(docs_x):
 
 training = numpy.array(training)
 output = numpy.array(output) 
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
